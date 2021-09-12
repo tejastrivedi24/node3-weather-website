59a8e58328c5ddd932b0982f0618c71e6dbb7bfe
Tejas Trivedi <tejastrivedi@Tejass-MacBook-Pro.local>
2021-09-12 18:16:35 +0530

    Init commit

---
+++ src/app.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const forecast = require('./utils/forecast.js')
+const app =express()
+const path = require('path')
+const hbs = require('hbs')
+
+app.use(express.static(path.join(__dirname,'../public')))
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+
+// app.get('',(req,res)=>{
+
+//     res.send('<h1>Hello express!</h1>')
+
+// })
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather app',
+        name:'Tejas'
+    })
+})
+
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:'Help',
+        name:'Tejas'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About',
+        name:'Tejas'
+    })
+})
+
+app.get('/weather',(req,res)=>{
+    if(!req.query.address){
+        return res.send({
+            error:'You must provide a search term'
+        })
+    }
+
+    forecast(req.query.address,(error,data)=>{
+        if(error){
+            return res.send({error})
+        }
+
+        res.send({
+            forecast:data,
+            
+            location:req.query.address
+
+        })
+    })
+    // res.json({
+    //     temperature: '32C',
+    //     humidity: 50,
+    //     address:req.query.address
+    // })
+})
+
+app.get('/products',(req,res)=>{
+
+    if(!req.query.search){
+        return res.send({
+            error:'You must provide a search term'
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products:[]
+    })
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title:'Error page',
+        name:'Tejas',
+        errorMessage:'Help article not found'
+    })
+})
+
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title:'404',
+        name:'Tejas',
+        errorMessage:'Page not found'
+    })
+})
+
+
+app.listen(3000,() => {
+    console.log('server is running on port 3000!')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,47 @@
+const { builtinModules } = require('module')
+const request = require('request')
+
+const forecast=(address,callback)=>{
+
+    request({ url:'http://api.weatherstack.com/current?access_key=d9fb1e655242347034fe922fce581538&query=' +address+ '&units=f', json:true }, (error,response)=>{
+
+        if(error){
+            callback('Make sure you are connected to the internet',undefined)
+        }
+        else if(response.body.error){
+    
+            callback('Unable to find location',undefined)
+    
+        }
+
+        // else if(response.body.error){
+        //     callback('Unable to find location',undefined)
+        // }
+     
+        else{
+            callback(undefined,{desc:response.body.current.weather_descriptions[0],temp:response.body.current.temperature,feelslike:response.body.current.feelslike})
+    
+        }
+    })
+
+}
+
+module.exports=forecast
+
+// const request = require('request')
+
+// const forecast = (latitude, longitude, callback) => {
+//     const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+
+//     request({ url, json: true }, (error, { body }) => {
+//         if (error) {
+//             callback('Unable to connect to weather service!', undefined)
+//         } else if (body.error) {
+//             callback('Unable to find location', undefined)
+//         } else {
+//             callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
+//         }
+//     })
+// }
+
+// module.exports = forecast
